8a087f10a663ee18c4f8287979990e60a1bac22f
MaximSolodkovVMSIS <gt6600502@gmail.com>
2024-04-02 00:38:47 +0300

    1

--- src/test/java/undertaken/lab1/entity/LanguageTest.java
+++ src/test/java/undertaken/lab1/entity/LanguageTest.java
@@ -2,15 +2,57 @@ package undertaken.lab1.entity;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 class LanguageTest {
 
     @Test
-    void testGetIdAndSetId() {
+    void testSetId() {
         Language language = new Language();
-        Long expectedId = 123L;
+        Long expectedId = 1L;
         language.setId(expectedId);
         assertEquals(expectedId, language.getId());
+    }
+
+    @Test
+    void testGetName() {
+        Language language = new Language();
+        String expectedName = "English";
+        language.setName(expectedName);
+        assertEquals(expectedName, language.getName());
+    }
+
+    @Test
+    void testSetName() {
+        Language language = new Language();
+        String expectedName = "English";
+        language.setName(expectedName);
+        assertEquals(expectedName, language.getName());
+    }
+
+    @Test
+    void testGetTextLanguages() {
+        Language language = new Language();
+        Text text1 = new Text();
+        Text text2 = new Text();
+        List<Text> expectedTextLanguages = new ArrayList<>();
+        expectedTextLanguages.add(text1);
+        expectedTextLanguages.add(text2);
+        language.setTextLanguages(expectedTextLanguages);
+        assertEquals(expectedTextLanguages, language.getTextLanguages());
+    }
+
+    @Test
+    void testSetTextLanguages() {
+        Language language = new Language();
+        Text text1 = new Text();
+        Text text2 = new Text();
+        List<Text> expectedTextLanguages = new ArrayList<>();
+        expectedTextLanguages.add(text1);
+        expectedTextLanguages.add(text2);
+        language.setTextLanguages(expectedTextLanguages);
+        assertEquals(expectedTextLanguages, language.getTextLanguages());
     }
 }
