dc560ebad88c583bf43517a2648ba5d1a8b32dc4
MaximSolodkovVMSIS <gt6600502@gmail.com>
2024-04-01 22:23:02 +0300

    1

---
+++ src/main/java/undertaken/lab1/dto/ParameterList.java
@@ -0,0 +1,15 @@
+package undertaken.lab1.dto;
+
+import java.util.List;
+
+public class ParameterList {
+    private List<String> parameters;
+
+    public List<String> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(List<String> parameters) {
+        this.parameters = parameters;
+    }
+}
