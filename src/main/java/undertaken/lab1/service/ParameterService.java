2f17105c68070375550795c58d2af8b63632801e
MaximSolodkovVMSIS <gt6600502@gmail.com>
2024-04-01 23:58:45 +0300

    1

--- src/main/java/undertaken/lab1/service/ParameterService.java
+++ src/main/java/undertaken/lab1/service/ParameterService.java
@@ -1,7 +1,5 @@
 package undertaken.lab1.service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import undertaken.lab1.dto.ParameterList;
 
@@ -10,13 +8,11 @@ import java.util.List;
 @Service
 public class ParameterService {
 
-    private static final Logger logger = LoggerFactory.getLogger(ParameterService.class);
-
     public void processParameters(ParameterList parameterList) {
         List<String> parameters = parameterList.getParameters();
         // Пример обработки параметров с использованием Java 8 Stream API
         parameters.stream()
                 .map(String::toUpperCase)
-                .forEach(logger::info);
+                .forEach(System.out::println);
     }
 }
