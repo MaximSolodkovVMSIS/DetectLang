e83616a6af69333444cc1bdd1b9522b326916b53
MaximSolodkovVMSIS <gt6600502@gmail.com>
2024-04-01 19:54:09 +0300

    1

--- src/test/java/undertaken/lab1/service/CrudOperationTest.java
+++ src/test/java/undertaken/lab1/service/CrudOperationTest.java
@@ -5,9 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import undertaken.lab1.entity.Language;
 import undertaken.lab1.entity.Text;
 import undertaken.lab1.repository.LanguageRepository;
 import undertaken.lab1.repository.TextRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -18,6 +23,9 @@ import static org.mockito.Mockito.times;
 class CrudOperationTest {
     @Mock
     private TextRepository textRepository;
+
+    @Mock
+    private LanguageRepository languageRepository;
 
     @InjectMocks
     private CrudOperation crudOperation;
@@ -85,10 +93,67 @@ class CrudOperationTest {
         verify(textRepository, never()).delete(any(Text.class));
     }
     @Test
-    void deleteLanguageAndText() {
+    void deleteLanguageAndText_LanguageFoundAndDeletedSuccessfully() {
+        String languageName = "English";
+        Language language = new Language();
+        language.setName(languageName);
+        List<Text> texts = new ArrayList<>();
+        Text text1 = new Text();
+        text1.setText("Hello");
+        texts.add(text1);
+        Text text2 = new Text();
+        text2.setText("World");
+        texts.add(text2);
+        language.setTextLanguages(texts);
+        when(languageRepository.findByName(languageName)).thenReturn(language);
+
+        // Act
+        String result = crudOperation.deleteLanguageAndText(languageName);
+
+        // Assert
+        assertEquals("Language and associated texts deleted successfully", result);
+        verify(textRepository, times(1)).deleteAll(texts);
+        verify(languageRepository, times(1)).delete(language);
     }
 
     @Test
-    void updateText() {
+    void deleteLanguageAndText_LanguageNotFound() {
+        String languageName = "UnknownLanguage";
+        when(languageRepository.findByName(languageName)).thenReturn(null);
+
+        // Act
+        String result = crudOperation.deleteLanguageAndText(languageName);
+
+        // Assert
+        assertEquals("Language not found", result);
+        verify(textRepository, never()).deleteAll(anyList());
+        verify(languageRepository, never()).delete(any(Language.class));
+    }
+
+    @Test
+    void updateText_TextFoundAndUpdatedSuccessfully() {
+        Long textID = 1L;
+        String newText = "Updated text";
+        Text text = new Text();
+        text.setId(textID);
+        when(textRepository.findById(textID)).thenReturn(Optional.of(text));
+
+        String result = crudOperation.updateText(textID, newText);
+
+        assertEquals("Text updated successfully", result);
+        assertEquals(newText, text.getText());
+        verify(textRepository, times(1)).save(text);
+    }
+
+    @Test
+    void updateText_TextNotFound() {
+        Long textID = 1L;
+        String newText = "Updated text";
+        when(textRepository.findById(textID)).thenReturn((Optional.empty()));
+
+        String result = crudOperation.updateText(textID, newText);
+
+        assertEquals("Text not found", result);
+        verify(textRepository, never()).save(any(Text.class));
     }
 }
