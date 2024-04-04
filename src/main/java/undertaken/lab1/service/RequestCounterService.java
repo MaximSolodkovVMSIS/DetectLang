d6285d03feb593cd7b8d00bd20cc2a3e16adf3e2
MaximSolodkovVMSIS <gt6600502@gmail.com>
2024-04-04 21:27:13 +0300

    1

---
+++ src/main/java/undertaken/lab1/service/RequestCounterService.java
@@ -0,0 +1,18 @@
+package undertaken.lab1.service;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class RequestCounterService {
+
+    private RequestCounterService() {}
+    private final static AtomicInteger REQUEST_COUNT = new AtomicInteger(0);
+
+    public static synchronized void incrementRequestCount() {
+        REQUEST_COUNT.incrementAndGet();
+    }
+
+    public static synchronized void getRequestCount() {
+        System.out.println(REQUEST_COUNT.get());
+    }
+
+}
